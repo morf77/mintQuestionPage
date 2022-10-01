5bf3bb4376fce4c0ba399dba8765fb53aee53040
morf77 <mhamadreza77@gmail.com>
2022-10-01 23:29:15 +0330

    Updates

---
+++ precache-manifest.571cb698b2d8ef80ed8e3a4a0cdcb0a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e8dfad8961d4deafede",
+    "url": "/mintQuestionPage/static/css/main.771e1965.chunk.css"
+  },
+  {
+    "revision": "2e8dfad8961d4deafede",
+    "url": "/mintQuestionPage/static/js/main.2e8dfad8.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/mintQuestionPage/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "429d8dc43bf5bb0a1c46",
+    "url": "/mintQuestionPage/static/js/2.429d8dc4.chunk.js"
+  },
+  {
+    "revision": "ca1cd5761899e2400d5f",
+    "url": "/mintQuestionPage/static/js/runtime~main.ca1cd576.js"
+  },
+  {
+    "revision": "442ef1c7d7f5bd40196860dd520b26a2",
+    "url": "/mintQuestionPage/static/media/right.442ef1c7.jpg"
+  },
+  {
+    "revision": "6bb31ff6ae6bc0f0dcf86f76a848ac0b",
+    "url": "/mintQuestionPage/index.html"
+  }
+];
